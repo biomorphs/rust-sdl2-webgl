10b2b9570a89b07ec5a50876eb04b53867ac818c
biomorphs <matt.hoyle@gmail.com>
2025-04-15 02:16:43 +0100

    Re-instated window resize event handler for sdl2

--- rust-sdl2-webgl/src/sdl2_context.rs
+++ rust-sdl2-webgl/src/sdl2_context.rs
@@ -53,6 +53,13 @@ pub fn run_sdl2_event_loop(mut context: SDL2Context, mut app_state: crate::app::
             for event in context.event_loop.poll_iter() {
                 match event {
                     sdl2::event::Event::Quit { .. } => running = false,
+                    sdl2::event::Event::Window { timestamp: _, window_id: _, win_event } => {
+                        if let sdl2::event::WindowEvent::Resized(w, h) = win_event      // detect window resize
+                        {
+                            context.window_width = w as u32;
+                            context.window_height = h as u32;
+                        }
+                    }
                     _ => {}
                 }
             }
